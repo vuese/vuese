e08679d53c1bd3efd3e56139e44273107dfdbc74
HcySunYang <HcySunYang@outlook.com>
2018-10-12 17:27:48 +0800

    tweak

--- bin/index.js
+++
@@ -1,4 +0,0 @@
-const path = require('path')
-
-const str = '2'
-console.log(path.basename('/a/b/c.js') + str)
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import parser, { ParserResult } from './parser'
-import Render, { RenderResult } from './render'
+import parser from './parser'
+import Render from './render'
 
-export default function(source: string): string {
-  const pr: ParserResult = parser(source)
-  const r = new Render(pr)
-  const renderRes: RenderResult = r.render()
-  let mdString = ''
-  if (renderRes.props) {
-    mdString += renderRes.props + '\n'
-  }
-  if (renderRes.events) {
-    mdString += renderRes.events + '\n'
-  }
-  if (renderRes.slots) {
-    mdString += renderRes.slots + '\n'
-  }
-  if (renderRes.methods) {
-    mdString += renderRes.methods + '\n'
-  }
-
-  return mdString
+export default {
+  parser,
+  Render
 }
