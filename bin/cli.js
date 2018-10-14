fbf03b75253446fb811dd7e531ecf12cdd392d3e
HcySunYang <HcySunYang@outlook.com>
2018-10-14 20:10:41 +0800

    tweak cli

--- bin/cli.js
+++ bin/cli.js
@@ -3,8 +3,6 @@
 const cac = require('cac')
 const JoyCon = require('joycon')
 const majo = require('majo')
-const path = require('path')
-const logger = require('log-horizon').create()
 
 const cli = cac()
 const joycon = new JoyCon({
@@ -20,14 +18,18 @@ joycon.addLoader({
 
 async function getConfig (flags) {
   const { path, data } = await joycon.load(['vuese.config.js', '.vueserc', 'package.json'])
-  const config = {}
+  const config = {
+    include: '**/*.vue',
+    exclude: [],
+    outDir: 'website',
+    markdownDir: 'components'
+  }
   if (path) Object.assign(config, data, flags)
   return config
 }
 
 cli.command('*', 'vuese cli', async (input, flags) => {
-  const config = await getConfig(flags)
-  require('../lib')(config)
+  cli.showHelp()
 })
 
 cli.command('gen', 'Generate target resources', async (input, flags) => {
@@ -35,22 +37,12 @@ cli.command('gen', 'Generate target resources', async (input, flags) => {
   const questions = require('./questions')
   const answser = await require('inquirer').prompt(questions)
   if (answser.genType === 'docute') require('../lib/genDocute')(config)
+  if (answser.genType === 'markdown') require('../lib')(config)
 })
 
 cli.command('serve', 'Serve generated docute website', async (input, flags) => {
   const config = await getConfig(flags)
-  const servePath = path.resolve(config.outDir)
-  const http = require('http')
-  const handler = require('serve-handler')
-  const server = http.createServer((req, res) => {
-    return handler(req, res, {
-      public: path.resolve(config.outDir)
-    })
-  })
-  
-  server.listen('5000', '127.0.0.1',() => {
-    logger.success(`Server running at http://127.0.0.1:5000/`);
-  })
+  require('../lib/server')(config)
 })
 
 cli.parse()
