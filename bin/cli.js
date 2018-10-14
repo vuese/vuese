9510b42e10e4dfd3de8352136de052a804f19cb0
HcySunYang <HcySunYang@outlook.com>
2018-10-14 16:25:59 +0800

    Generate documentation and add loggers

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,8 @@
 const cac = require('cac')
 const JoyCon = require('joycon')
 const majo = require('majo')
+const path = require('path')
+const logger = require('log-horizon').create()
 
 const cli = cac()
 const joycon = new JoyCon({
@@ -16,11 +18,39 @@ joycon.addLoader({
   }
 })
 
-cli.command('*', 'vuese cli', async (input, flags) => {
+async function getConfig (flags) {
   const { path, data } = await joycon.load(['vuese.config.js', '.vueserc', 'package.json'])
   const config = {}
   if (path) Object.assign(config, data, flags)
-  require('./lib')(config)
+  return config
+}
+
+cli.command('*', 'vuese cli', async (input, flags) => {
+  const config = await getConfig(flags)
+  require('../lib')(config)
+})
+
+cli.command('gen', 'Generate target resources', async (input, flags) => {
+  const config = await getConfig(flags)
+  const questions = require('./questions')
+  const answser = await require('inquirer').prompt(questions)
+  if (answser.genType === 'docute') require('../lib/genDocute')(config)
+})
+
+cli.command('serve', 'Serve generated docute website', async (input, flags) => {
+  const config = await getConfig(flags)
+  const servePath = path.resolve(config.outDir)
+  const http = require('http')
+  const handler = require('serve-handler')
+  const server = http.createServer((req, res) => {
+    return handler(req, res, {
+      public: path.resolve(config.outDir)
+    })
+  })
+  
+  server.listen('5000', '127.0.0.1',() => {
+    logger.success(`Server running at http://127.0.0.1:5000/`);
+  })
 })
 
 cli.parse()
---
+++ bin/questions.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    type: 'list',
+    name: 'genType',
+    message: 'Select the target to generate',
+    default: 'docute',
+    choices: [
+      {
+        name: 'Docute - The fastest way to create a documentation site for your project.',
+        value: 'docute'
+      },
+      {
+        name: 'Only markdown.',
+        value: 'markdown'
+      }
+    ]
+  }
+]
