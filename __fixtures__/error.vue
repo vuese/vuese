c9d05015fc023f8ca70ca19d4b4e6af993e56b24
HcySunYang <HcySunYang@outlook.com>
2018-10-30 12:21:38 +0800

    improve error reporting

---
+++ __fixtures__/error.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Form header -->
+  <slot name="header">
+    <!-- `<th>title</th>` -->
+    <th>title</th>
+  </slot>
+</template>
+
+<script>
+export default {
+  props: {
+    // The name of the form, up to 8 characters
+    name: {
+      type: [String, Number],
+      required: true,
+      validator () {}
+    }
+  },
+  methods: {
+    // @vuese
+    // Used to manually clear the form
+    clear () {
+      // Fire when the form is cleared
+      // @arg The argument is a boolean value representing xxx
+      this.$emit('onclear', true)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
