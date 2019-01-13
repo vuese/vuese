919593c9fc031931d250fe768d6d897b5e441a04
HcySunYang <HcySunYang@outlook.com>
2019-01-13 18:02:06 +0800

    build: tweak external

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import typescript from 'rollup-plugin-typescript2'
-import moduleResolve from 'rollup-plugin-node-resolve'
 import isBuiltinModule from 'is-builtin-module'
 
 function resolveInput(projectDir) {
@@ -11,19 +10,24 @@ function resolveOnput(projectDir) {
   return path.resolve('packages', `${projectDir}/dist/index.js`)
 }
 
+const PKG_DIR = process.env.PKG_DIR
+const pkgMeta = require(path.resolve(`packages`, `${PKG_DIR}/package.json`))
+
 export default {
-  input: resolveInput(process.env.PKG_DIR),
+  input: resolveInput(PKG_DIR),
   external(id) {
-    return id.includes('node_modules') || id.includes('@vuese')
+    return (
+      (pkgMeta.dependencies && !!pkgMeta.dependencies[id]) ||
+      id === 'prismjs/components'
+    )
   },
   plugins: [
     typescript({
       cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache')
-    }),
-    moduleResolve()
+    })
   ],
   output: {
-    file: resolveOnput(process.env.PKG_DIR),
+    file: resolveOnput(PKG_DIR),
     format: 'cjs'
   },
   onwarn(warning, warn) {
