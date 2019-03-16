d2d2bf1aaa0771995f51edaa80cae4a267d49614
elevatebart <ledouxb@me.com>
2019-03-16 16:26:32 -0500

    test(parser): add test for use of v-if

---
+++ packages/parser/__test__/__fixtures__/slotWithVif.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a v-if="shouldDefaultSlot">noDefaultSlot</a>
+    <div v-else>
+      <!-- default slot -->
+      <slot>
+        <!-- Default Slot Content -->
+        <p></p>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    shouldDefaultSlot: Boolean
+  }
+}
+</script>
+
+<style>
+</style>
