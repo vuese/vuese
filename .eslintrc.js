cf304d6c704f3e5eec684c844ce3daddd1e56288
IWANABETHATGUY <974153916@qq.com>
2020-04-17 00:11:01 +0800

    :nut_and_bolt: update .eslintrc and fix some lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,20 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "@typescript-eslint/tslint"
-    ],
-    "rules": {
-        "@typescript-eslint/tslint/config": [
-            "error",
-            {
-                "rules": {
-                    "prettier": [
-                        true,
-                        ".prettierrc"
-                    ]
-                }
-            }
-        ]
-    }
-};
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-use-before-define': 0
+  },
+  ignorePatterns: ['*.vue']
+}
