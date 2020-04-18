83f4123f30435fb49a171fe87c24740122f451e7
IWANABETHATGUY <974153916@qq.com>
2020-04-18 17:32:30 +0800

    :star2: (cli) keep folder

--- samples/components/Component.vue
+++ samples/components/Component.vue
@@ -1,65 +1,22 @@
 <template>
-  <div id="main">
+  <div>
+    <!-- Form header -->
+    <slot name="header">
+      <!-- `<th>title</th>` -->
+      <th>title</th>
+    </slot>
   </div>
 </template>
 
 <script>
-// Component item
+/* eslint-disable camelcase */
 export default {
-  name: "Component",
-  props: {
-    // The component title
-    title: String,
-  },
-  data: () => ({
-    /**
-     * @vuese
-     * A string value that does something
-     */
-    vueStringData: "data",
-    /**
-     * @vuese
-     * A function that does nothing
-     */
-    localFunction : () => {}
-  }),
-  computed: {
-    /**
-     * @vuese
-     * Reverses string
-     * @type Array
-     */
-    reversedMessage: function () {
-      return this.vueStringData.split('').reverse().join('')
-    },
-    /**
-     * @vuese
-     * Returns a value from store
-     * @type Array
-     */
-    storeValue() {
-      return this.$store.state.value;
-    },
-  },
-  watch: {
-    /**
-     * @vuese
-     * Watches question
-     * @arg val - the value
-     */
-    question: function(val) {
-      this.debouncedGetAnswer()
-    },
-  },
-  methods: {
-    /**
-     * @vuese
-     * Do something
-     * @arg val - the value
-     */
-    OnDoSomething(value) {
-      value++
-    },
-  }
-};
+    name: 'CustomComponent',
+    methods: {
+      test() {
+        // event desc
+        someVar && this.$emit('some-event')
+      }
+    }
+}
 </script>
---
+++ samples/components/Fuck.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  methods: {
+    test() {
+      this.$emit('fuck')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
