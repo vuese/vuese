c25a6d84b0e689a5172da1194600c75c9ecbb32b
IWANABETHATGUY <974153916@qq.com>
2020-04-19 12:33:14 +0800

    :beetle: (cli) revert samples/component.vue

--- samples/components/Component.vue
+++ samples/components/Component.vue
@@ -1,22 +1,65 @@
 <template>
-  <div>
-    <!-- Form header -->
-    <slot name="header">
-      <!-- `<th>title</th>` -->
-      <th>title</th>
-    </slot>
+  <div id="main">
   </div>
 </template>
 
 <script>
-/* eslint-disable camelcase */
+// Component item
 export default {
-    name: 'CustomComponent',
-    methods: {
-      test() {
-        // event desc
-        someVar && this.$emit('some-event')
-      }
-    }
-}
+  name: "Component",
+  props: {
+    // The component title
+    title: String,
+  },
+  data: () => ({
+    /**
+     * @vuese
+     * A string value that does something
+     */
+    vueStringData: "data",
+    /**
+     * @vuese
+     * A function that does nothing
+     */
+    localFunction : () => {}
+  }),
+  computed: {
+    /**
+     * @vuese
+     * Reverses string
+     * @type Array
+     */
+    reversedMessage: function () {
+      return this.vueStringData.split('').reverse().join('')
+    },
+    /**
+     * @vuese
+     * Returns a value from store
+     * @type Array
+     */
+    storeValue() {
+      return this.$store.state.value;
+    },
+  },
+  watch: {
+    /**
+     * @vuese
+     * Watches question
+     * @arg val - the value
+     */
+    question: function(val) {
+      this.debouncedGetAnswer()
+    },
+  },
+  methods: {
+    /**
+     * @vuese
+     * Do something
+     * @arg val - the value
+     */
+    OnDoSomething(value) {
+      value++
+    },
+  }
+};
 </script>
