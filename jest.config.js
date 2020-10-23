19f87fd5827a2213140229f6d0b06a8d81de88af
IWANABETHATGUY <974153916@qq.com>
2020-10-23 16:46:53 +0800

    perf: ⚡️ ship ts-jest to @swc-node/jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 /**@type{import("ts-jest")} */
 module.exports = {
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': '@swc-node/jest'
   },
   testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@vuese/(.*?)$': '<rootDir>/packages/$1/lib'
-  },
-  globals: {
-    'ts-jest': {
-      diagnostics: false
-    }
   }
 }
